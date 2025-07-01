3dda3260d5a08dc8bebf73ee3e3be24e741a4446
brianrego03 <brian.rego03@gmail.com>
2025-07-01 16:22:58 +0530

    Initialize port

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ app.use(express.static(assetsPath));
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+
+app.use(express.urlencoded({extended:true}));
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log("we good");
+})
