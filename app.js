78b00b4728dc19415333b19cd28e13ad767355c9
brianrego03 <brian.rego03@gmail.com>
2025-07-02 12:50:06 +0530

    Set up session storage

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const localStrategy =require("passport-local").Strategy;
+const pgSession=require("connect-pg-simple")(session);
+const pool = require("./db/pool");
 
 const app =express();
 
@@ -19,6 +21,26 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 
 app.use(express.urlencoded({extended:true}));
+
+app.use(session(
+    {
+        store: new pgSession({
+            pool:pool,
+            tableName:"session"
+        }),
+        secret:"topStuff",
+        resave:false,
+        saveUninitialized:false,
+        cookie:{
+            maxAge:24*60*60*1000
+        }
+    }
+
+));
+
+app.use(passport.session());
+
+
 
 app.use("/",indexRouter);
 app.use("/signup",signUpRouter);
