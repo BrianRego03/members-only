ec9b1a1a43f09b0db0e34e2b1aa842effa8d7819
brianrego03 <brian.rego03@gmail.com>
2025-07-02 10:30:55 +0530

    Create controller function to post signup

--- controllers/signUpControllers.js
+++ controllers/signUpControllers.js
@@ -1,8 +1,22 @@
-
+const bcrypt = require("bcryptjs");
 
 
 const loadSignUp=(req,res)=>{
     res.render("signUp");
 }
 
-module.exports={loadSignUp};
+const saveSignUp=async(req,res,next)=>{
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        res.redirect("/");
+    }
+    catch(error) {
+        console.error(error);
+        next(error);
+    }
+    
+
+}
+
+module.exports={loadSignUp,saveSignUp};
